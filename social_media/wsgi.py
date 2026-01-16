a5da09f947e38b6341c4dc4882e3cfbce3ad0c36
riken4 <152069258+riken4@users.noreply.github.com>
2026-01-16 12:38:05 +0545

    Update wsgi.py

--- social_media/wsgi.py
+++ social_media/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for social_media project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
+import sys
+path = '/home/riken4/social-media'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media.settings')
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'social_media.settings'
 
 application = get_wsgi_application()
